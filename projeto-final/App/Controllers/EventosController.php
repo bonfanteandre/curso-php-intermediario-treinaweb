b8a51cb4b6afd3f236fe5446e68d4e39bb34387e
André Bonfante <andre458bonfante@gmail.com>
2018-10-02 22:00:03 -0300

    Finalizando o projeto final

--- projeto-final/App/Controllers/EventosController.php
+++ projeto-final/App/Controllers/EventosController.php
@@ -14,30 +14,43 @@ class EventosController extends Controller
 
     public function __construct()
     {
-        
+        $this->pessoa = new Pessoa;
+        $this->evento = new Evento;
+        $this->pessoaEvento = new PessoaEvento;
     }
 
     public function index()
     {   
-        return $this->render('eventos/index');
+        $id = input()->get('pessoa');
+        
+        $pessoa = $this->pessoa->getById($id);
+        $eventos = $this->pessoa->getEventos($id);
+
+        return $this->render('eventos/index', [
+            'pessoa' => $pessoa,
+            'eventos' => $eventos 
+        ]);
     }
 
     public function cadastrar()
     {
-//        $idEvento = input()->get('evento', false);
-//        $idPessoa = input()->get('pessoa');
-//
-//        if(!$idPessoa) {
-//            return redirect()->route('index');
-//        }
-//
-//        $evento = null;
-//
-//        if($idEvento) {
-//            $evento = $this->evento->getById($idEvento);
-//        }
+       $idEvento = input()->get('evento', false);
+       $idPessoa = input()->get('pessoa');
 
-        return $this->render('eventos/cadastrar');
+       if(!$idPessoa) {
+           return redirect()->route('index');
+       }
+
+       $evento = null;
+
+       if($idEvento) {
+           $evento = $this->evento->getById($idEvento);
+       }
+
+        return $this->render('eventos/cadastrar', [
+            'pessoa' => $this->pessoa->getById($idPessoa),
+            'evento' => $evento
+        ]);
     }
 
     public function salvar()
@@ -54,17 +67,17 @@ class EventosController extends Controller
             'descricao' => input()->get('descricao')
         ];
 
-//        // Atualizar
-//        if($idEvento) {
-//            $this->evento->updateById($idEvento, $fields);
-//
-//            session()->put('_sucesso', 'O evento foi atualizado com sucesso');
-//
-//            return redirect()->route('eventos.index', [
-//                'evento' => $idEvento,
-//                'pessoa' => $idPessoa
-//            ]);
-//        }
+       // Atualizar
+       if($idEvento) {
+           $this->evento->updateById($idEvento, $fields);
+
+           session()->put('_sucesso', 'O evento foi atualizado com sucesso');
+
+           return redirect()->route('eventos.index', [
+               'evento' => $idEvento,
+               'pessoa' => $idPessoa
+           ]);
+       }
 
         // Cadastra o evento
         $idEventoCadastrado = $this->evento->insert($fields);
